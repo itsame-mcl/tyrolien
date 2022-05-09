f02c870a33253ee5747e78869c20b8a7f7890d41
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-05-09 16:41:55 +0200

    Définition Store Articles

--- src/js/store.js
+++ src/js/store.js
@@ -3,33 +3,17 @@ import { createStore } from 'framework7/lite';
 
 const store = createStore({
   state: {
-    products: [
-      {
-        id: '1',
-        title: 'Apple iPhone 8',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
-      },
-      {
-        id: '2',
-        title: 'Apple iPhone 8 Plus',
-        description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
-      },
-      {
-        id: '3',
-        title: 'Apple iPhone X',
-        description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
-      },
-    ]
-  },
-  getters: {
-    products({ state }) {
-      return state.products;
-    }
+   articles : []
   },
   actions: {
-    addProduct({ state }, product) {
-      state.products = [...state.products, product];
+    loadArticles({ state }, {path}) {
+      fetch(path).then(res => res.json()).then(articles => state.articles = articles);
     },
   },
+  getters: {
+    articles({ state }) {
+      return state.articles;
+    }
+  }
 })
 export default store;

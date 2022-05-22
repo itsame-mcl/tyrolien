83b4235aabfd7870bc127ea7623487679ceb6f04
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-05-22 12:29:46 +0200

    Vue principale (composants uniquement)

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -29,9 +29,6 @@ import routes from '../js/routes';
 import store from '../js/store';
 
 const MyApp = () => {
-  // Login screen demo data
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const device = getDevice();
   // Framework7 Parameters
   const f7params = {
@@ -56,11 +53,7 @@ const MyApp = () => {
         androidOverlaysWebView: false,
       },
   };
-  const alertLoginData = () => {
-    f7.dialog.alert('Username: ' + username + '<br>Password: ' + password, () => {
-      f7.loginScreen.close();
-    });
-  }
+
   f7ready(() => {
 
     // Init capacitor APIs (see capacitor-app.js)
@@ -74,76 +67,9 @@ const MyApp = () => {
   return (
     <App { ...f7params } >
 
-        {/* Left panel with cover effect*/}
-        <Panel left cover dark>
-          <View>
-            <Page>
-              <Navbar title="Left Panel"/>
-              <Block>Left panel content goes here</Block>
-            </Page>
-          </View>
-        </Panel>
-
-
-        {/* Right panel with reveal effect*/}
-        <Panel right reveal dark>
-          <View>
-            <Page>
-              <Navbar title="Right Panel"/>
-              <Block>Right panel content goes here</Block>
-            </Page>
-          </View>
-        </Panel>
-
-
         {/* Your main view, should have "view-main" class */}
         <View main className="safe-areas" url="/" />
 
-      {/* Popup */}
-      <Popup id="my-popup">
-        <View>
-          <Page>
-            <Navbar title="Popup">
-              <NavRight>
-                <Link popupClose>Close</Link>
-              </NavRight>
-            </Navbar>
-            <Block>
-              <p>Popup content goes here.</p>
-            </Block>
-          </Page>
-        </View>
-      </Popup>
-
-      <LoginScreen id="my-login-screen">
-        <View>
-          <Page loginScreen>
-            <LoginScreenTitle>Login</LoginScreenTitle>
-            <List form>
-              <ListInput
-                type="text"
-                name="username"
-                placeholder="Your username"
-                value={username}
-                onInput={(e) => setUsername(e.target.value)}
-              ></ListInput>
-              <ListInput
-                type="password"
-                name="password"
-                placeholder="Your password"
-                value={password}
-                onInput={(e) => setPassword(e.target.value)}
-              ></ListInput>
-            </List>
-            <List>
-              <ListButton title="Sign In" onClick={() => alertLoginData()} />
-              <BlockFooter>
-                Some text about login information.<br />Click "Sign In" to close Login Screen
-              </BlockFooter>
-            </List>
-          </Page>
-        </View>
-      </LoginScreen>
     </App>
   )
 }

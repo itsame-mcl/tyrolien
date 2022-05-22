a02bcf483dbc1631c936efcd88561ee456e73bf1
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-05-22 13:15:22 +0200

    Exemple de réglage d'une option

--- src/pages/options.jsx
+++ src/pages/options.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Page, Navbar, Block, BlockTitle } from 'framework7-react';
+import React, { useEffect } from 'react';
+import { useStore, Page, Navbar, Block, BlockTitle, List, ListItem } from 'framework7-react';
+import store from '../js/store';
 
-const OptionsPage = () => (
+const OptionsPage = () => {
+    const articles_ordonnes = useStore("articles_ordonnes");
+    return(
     <Page>
         <Navbar title="Options" backLink="Back" />
         <BlockTitle>Options</BlockTitle>
@@ -10,11 +13,15 @@ const OptionsPage = () => (
             <p>Laudantium neque magnam vitae nemo quam commodi, in cum dolore obcaecati laborum, excepturi harum, optio qui, consequuntur? Obcaecati dolor sequi nesciunt culpa quia perspiciatis, reiciendis ex debitis, ut tenetur alias.</p>
         </Block>
         <Block strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni molestiae laudantium dignissimos est nobis delectus nemo ea alias voluptatum architecto, amet similique, saepe iste consectetur in repellat ut minus quibusdam!</p>
-            <p>Molestias et distinctio porro nesciunt ratione similique, magni doloribus, rerum nobis, aliquam quae reiciendis quasi modi. Nam a recusandae, fugiat in ea voluptates fuga eius, velit corrupti reprehenderit dignissimos consequatur!</p>
-            <p>Blanditiis, cumque quo adipisci. Molestiae, dolores dolorum quos doloremque ipsa ullam eligendi commodi deserunt doloribus inventore magni? Ea mollitia veniam nostrum nihil, iusto doloribus a at! Ea molestiae ullam delectus!</p>
+            <List>
+                <ListItem checkbox
+                          value="articles_ordonnes"
+                          checked={articles_ordonnes}
+                          title="Ordonner les articles par prix croissant"
+                onChange={(e) => store.dispatch("setArticlesOrdonnes", {value: e.target.checked})}></ListItem>
+            </List>
         </Block>
-    </Page>
-);
+    </Page>);
+};
 
 export default OptionsPage;

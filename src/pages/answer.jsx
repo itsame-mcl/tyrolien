83af1c32edf8277f2e322507ff4e35a20c8e55b0
Lucas Macaux <lucas0203@orange.fr>
2022-05-23 09:01:40 +0200

    Màj de icons.css + Mise en forme jsx files

--- src/pages/answer.jsx
+++ src/pages/answer.jsx
@@ -4,34 +4,34 @@ import store from '../js/store';
 
 const AnswerPage = (props) => {
     const question = store.getters.article_actuel.value;
-    const [tauxErreur, setTauxErreur] = useState(store.getters.erreur_actuelle.value * 100/store.getters.erreur_maximum.value);
+    const [tauxErreur, setTauxErreur] = useState(store.getters.erreur_actuelle.value * 100 / store.getters.erreur_maximum.value);
     const [ready, setReady] = useState(false);
 
     useEffect(() => {
         const timer = setTimeout(() => {
             const erreur = Math.abs(Math.floor(question.price.amount) - parseInt(props.proposition));
-            setTauxErreur((store.getters.erreur_actuelle.value + erreur) * 100/store.getters.erreur_maximum.value);
-            store.dispatch("setErreurActuelle", {value: store.getters.erreur_actuelle.value + erreur});
+            setTauxErreur((store.getters.erreur_actuelle.value + erreur) * 100 / store.getters.erreur_maximum.value);
+            store.dispatch("setErreurActuelle", { value: store.getters.erreur_actuelle.value + erreur });
             setReady(true);
-        },2000);
+        }, 2000);
         return () => clearTimeout(timer);
     }, [])
 
-  return (
-    <Page>
-        <Navbar title={"Objet " + store.getters.question_actuelle.value + " / " + store.getters.nombre_questions.value}
+    return (
+        <Page>
+            <Navbar title={"Objet " + store.getters.question_actuelle.value + " / " + store.getters.nombre_questions.value}
                 backLink="Back" backLinkUrl="/" backLinkForce={true} />
-        <Block>
-            <Progressbar progress={tauxErreur}/>
-        </Block>
-        <Block strong>
-            <Row>{question.title}</Row>
-            <Row>{"Votre proposition : " + props.proposition + "." + (question.price.amount * 100) % 100 + " €"}</Row>
-            <Row>{"Bonne réponse : " + question.price.amount + " €"}</Row>
-        </Block>
-        <Button fill raised href="/next/" style={{display: ready ? "block": "none"}}>Continuer</Button>
-    </Page>
-  );
+            <Block>
+                <Progressbar progress={tauxErreur} />
+            </Block>
+            <Block strong>
+                <Row>{question.title}</Row>
+                <Row>{"Votre proposition : " + props.proposition + "." + (question.price.amount * 100) % 100 + " €"}</Row>
+                <Row>{"Bonne réponse : " + question.price.amount + " €"}</Row>
+            </Block>
+            <Button fill raised href="/next/" style={{ display: ready ? "block" : "none" }}>Continuer</Button>
+        </Page>
+    );
 }
 
 export default AnswerPage;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,37 +10,37 @@ import {
 } from 'framework7-react';
 
 const HomePage = () => (
-  <Page name="home">
-    {/* Top Navbar */}
-      <Navbar sliding="{false}">
-          <NavTitle>Tyrolien</NavTitle>
-      </Navbar>
-    {/* Toolbar */}
-    {/* Page content */}
+    <Page name="home">
+        {/* Top Navbar */}
+        <Navbar sliding="{false}">
+            <NavTitle>Tyrolien</NavTitle>
+        </Navbar>
+        {/* Toolbar */}
+        {/* Page content */}
 
-    <Block>
-        <Row nogap>
-            <Col width="25"></Col>
-            <Col width="50">
-                <Button fill raised href="/new_game/">Jouer</Button>
-            </Col>
-            <Col width="25"></Col>
-        </Row>
-        <Row nogap>
-            <Col width="25"></Col>
-            <Col width="50">
-                <Button fill raised href="/high_scores/">Meilleurs Scores</Button>
-            </Col>
-            <Col width="25"></Col>
-        </Row>
-        <Row nogap>
-            <Col width="25"></Col>
-            <Col width="50">
-                <Button fill raised href="/options/">Options</Button>
-            </Col>
-            <Col width="25"></Col>
-        </Row>
-    </Block>
-  </Page>
+        <Block>
+            <Row nogap>
+                <Col width="25"></Col>
+                <Col width="50">
+                    <Button fill raised href="/new_game/">Jouer</Button>
+                </Col>
+                <Col width="25"></Col>
+            </Row>
+            <Row nogap>
+                <Col width="25"></Col>
+                <Col width="50">
+                    <Button fill raised href="/high_scores/">Meilleurs Scores</Button>
+                </Col>
+                <Col width="25"></Col>
+            </Row>
+            <Row nogap>
+                <Col width="25"></Col>
+                <Col width="50">
+                    <Button fill raised href="/options/">Options</Button>
+                </Col>
+                <Col width="25"></Col>
+            </Row>
+        </Block>
+    </Page>
 );
 export default HomePage;

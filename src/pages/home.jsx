eb4d2e2f71ee30f90911be2d02de58db03a440ea
Lucas Macaux <lucas0203@orange.fr>
2022-05-23 13:07:19 +0200

    Ajout d'icons sur la page d'accueil

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,46 +1,46 @@
 import React from 'react';
 import {
-    Page,
-    Navbar,
-    NavTitle,
-    Block,
-    Row,
-    Col,
-    Button
+  Page,
+  Navbar,
+  NavTitle,
+  Block,
+  Row,
+  Col,
+  Button
 } from 'framework7-react';
 
 const HomePage = () => (
-    <Page name="home">
-        {/* Top Navbar */}
-        <Navbar sliding="{false}">
-            <NavTitle>Tyrolien</NavTitle>
-        </Navbar>
-        {/* Toolbar */}
-        {/* Page content */}
+  <Page name="home">
+    {/* Top Navbar */}
+    <Navbar sliding="{false}">
+      <NavTitle>Tyrolien</NavTitle>
+    </Navbar>
+    {/* Toolbar */}
+    {/* Page content */}
 
-        <Block>
-            <Row nogap>
-                <Col width="25"></Col>
-                <Col width="50">
-                    <Button fill raised href="/new_game/">Jouer</Button>
-                </Col>
-                <Col width="25"></Col>
-            </Row>
-            <Row nogap>
-                <Col width="25"></Col>
-                <Col width="50">
-                    <Button fill raised href="/high_scores/">Meilleurs Scores</Button>
-                </Col>
-                <Col width="25"></Col>
-            </Row>
-            <Row nogap>
-                <Col width="25"></Col>
-                <Col width="50">
-                    <Button fill raised href="/options/">Options</Button>
-                </Col>
-                <Col width="25"></Col>
-            </Row>
-        </Block>
-    </Page>
+    <Block>
+      <Row nogap>
+        <Col width="25"></Col>
+        <Col width="50">
+          <Button fill raised href="/new_game/">Jouer</Button>
+        </Col>
+        <Col width="25"></Col>
+      </Row>
+      <Row nogap>
+        <Col width="25"></Col>
+        <Col width="50">
+          <Button fill raised href="/high_scores/"><span class="material-icons">leaderboard</span>Meilleurs Scores</Button>
+        </Col>
+        <Col width="25"></Col>
+      </Row>
+      <Row nogap>
+        <Col width="25"></Col>
+        <Col width="50">
+          <Button fill raised href="/options/"><span class="material-icons">settings</span>Options</Button>
+        </Col>
+        <Col width="25"></Col>
+      </Row>
+    </Block>
+  </Page>
 );
 export default HomePage;

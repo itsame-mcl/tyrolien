3c17ea37f1852b827e57420b26082e209d98e16f
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-05-25 01:03:21 +0200

    Essais de centrage du texte

--- src/pages/answer.jsx
+++ src/pages/answer.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Haptics, ImpactStyle } from '@capacitor/haptics';
-import {Howl, Howler} from 'howler';
+import { Howl, Howler } from 'howler';
 import {
     Page,
     Navbar,
@@ -22,11 +22,11 @@ const AnswerPage = (props) => {
     const [ready, setReady] = useState(false);
     const isInitialMount = useRef(true);
 
-    const correctSound = new Howl({src: ["/sounds/correct.wav"]});
-    const erreurSound = new Howl({src: ["/sounds/erreur.wav"]});
-    const criSound = new Howl({src: ["/sounds/cri.wav"]});
-    const tyrolienSong = new Howl({src: ["/music/yodel-long.mp3"]});
-    const tyrolienShortSong = new Howl({src: ["/music/yodel-court.mp3"]});
+    const correctSound = new Howl({ src: ["/sounds/correct.wav"] });
+    const erreurSound = new Howl({ src: ["/sounds/erreur.wav"] });
+    const criSound = new Howl({ src: ["/sounds/cri.wav"] });
+    const tyrolienSong = new Howl({ src: ["/music/yodel-long.mp3"] });
+    const tyrolienShortSong = new Howl({ src: ["/music/yodel-court.mp3"] });
 
     const hapticsImpactHeavy = async () => {
         await Haptics.impact({ style: ImpactStyle.Heavy });
@@ -41,11 +41,11 @@ const AnswerPage = (props) => {
     useEffect(() => {
         const timer = setTimeout(() => {
             const erreur = Math.abs(Math.floor(question.price.amount) - parseInt(props.proposition));
-            setIterations(Math.min(erreur, erreur_maximum-erreur_affichee+1));
+            setIterations(Math.min(erreur, erreur_maximum - erreur_affichee + 1));
             store.dispatch("setErreurActuelle", { value: store.getters.erreur_actuelle.value + erreur });
             if (erreur > 0) {
                 erreurSound.play();
-                if(erreur_maximum-erreur_affichee>10) {
+                if (erreur_maximum - erreur_affichee > 10) {
                     tyrolienSong.play();
                 } else {
                     tyrolienShortSong.play();
@@ -61,14 +61,14 @@ const AnswerPage = (props) => {
         } else {
             if (iterations > 0) {
                 const timer = setTimeout(() => {
-                    if (reponse_affichee < question.price.amount){
+                    if (reponse_affichee < question.price.amount) {
                         setReponseAffichee(reponse_affichee + 1);
                     } else {
                         setReponseAffichee(reponse_affichee - 1);
                     }
 
                     setErreurAffichee(erreur_affichee + 1);
-                    if(erreur_maximum-erreur_affichee>10) {
+                    if (erreur_maximum - erreur_affichee > 10) {
                         hapticsImpactMedium();
                     } else {
                         hapticsImpactHeavy();
@@ -79,7 +79,7 @@ const AnswerPage = (props) => {
             }
             else {
                 Howler.stop();
-                if(erreur_affichee <= erreur_maximum) {
+                if (erreur_affichee <= erreur_maximum) {
                     correctSound.play();
                 }
                 else {
@@ -96,14 +96,24 @@ const AnswerPage = (props) => {
         <Page>
             <Navbar title={"Objet " + store.getters.question_actuelle.value + " / " + store.getters.nombre_questions.value}
                 backLink="Back" backLinkUrl="/" backLinkForce={true} />
-            <Row>
-                <Col width="100"><Tyrolien actuel={erreur_affichee} max={erreur_maximum} /></Col>
-            </Row>
-            <Block strong>
-                <Row>{question.title}</Row>
-                <Row><span style={{fontSize: "x-large"}}>{reponse_affichee.toFixed(2) + " €"}</span></Row>
-            </Block>
-            <Button fill raised href="/next/" style={{ display: ready ? "block" : "none" }}>Continuer</Button>
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <Row nogap>
+                <Col width="100" medium="100" large="50" style={{textAlign:'center'}}>
+                    <Tyrolien actuel={erreur_affichee} max={erreur_maximum} />
+                </Col>
+                    <Col width="100" medium="100" large="35">
+                        <Block strong>
+                            <Row style={{ justifyContent: 'center' }}>{question.title}</Row>
+                            <Row style={{ justifyContent: 'center' }}><span style={{ fontSize: "x-large" }}>{reponse_affichee.toFixed(2) + " €"}</span></Row>
+                        </Block>
+                    </Col>
+                </Row>
+            </div>
+            <Button small fill round raised href="/next/" style={{ display: ready ? "block" : "none" }}>Continuer</Button>
         </Page>
     );
 }
--- src/pages/question.jsx
+++ src/pages/question.jsx
@@ -11,28 +11,35 @@ const QuestionPage = () => {
             <Navbar title={"Objet " + store.getters.question_actuelle.value + " / " + store.getters.nombre_questions.value}
                 backLink="Back" backLinkUrl="/" backLinkForce={true} />
             <Block strong>
-                <Row nogap>
-                    {question.title}
-                </Row>
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                    <Row nogap>
+                        {question.title}
+                    </Row>
+                </div>
             </Block>
-            <div style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}>
-                <img src={"images/" + question._id.$oid + ".jpg"} height={500} />
-            </div>
-            <Row>
-                <Col width="5"></Col>
-                <Col width="25">
-                    <Stepper value={answer} min={10} max={99} step={1} autorepeat={true}
-                             manualInputMode={true} decimalPoint={0} onStepperChange={(e) => setAnswer(e)} />
-                    <span style={{fontSize: "x-large"}}>{"." + (question.price.amount % 1).toFixed(2).slice(-2) + " €"}</span>
-                </Col>
-                <Col width="50">
-                    <Button fill raised href={"/answer/" + answer + "/"}>Valider</Button>
-                </Col>
-            </Row>
+            <Block>
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                    <Row nogap>
+                    <Col width="100" medium="55" large="50">
+                        <img src={"images/" + question._id.$oid + ".jpg"} width={'50%'} />
+                    </Col>
+                    <Col width="100" medium="35" large="40" style={{ marginLeft: '10%' }}>
+                        <Stepper value={answer} min={10} max={99} step={1} autorepeat={true}
+                            manualInputMode={true} decimalPoint={0} onStepperChange={(e) => setAnswer(e)} />
+                        <span style={{ fontSize: "large" }}>{"." + (question.price.amount % 1).toFixed(2).slice(-2) + " €"}</span>
+                        <Button fill raised href={"/answer/" + answer + "/"}>Valider</Button>
+                    </Col>
+                    </Row>
+                </div>
+            </Block>
         </Page>
     );
 }

9a4b7a7687def67ee3879aa0ec31bd9bc3bb7042
Lucas Macaux <lucas0203@orange.fr>
2022-05-24 15:58:31 +0200

    Taille fixe pour les images

--- src/pages/question.jsx
+++ src/pages/question.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Page, Navbar, Row, Col, Block, Stepper, Button } from 'framework7-react';
 import store from '../js/store';
 
@@ -9,26 +9,26 @@ const QuestionPage = () => {
     return (
         <Page>
             <Navbar title={"Objet " + store.getters.question_actuelle.value + " / " + store.getters.nombre_questions.value}
-                    backLink="Back" backLinkUrl="/" backLinkForce={true} />
+                backLink="Back" backLinkUrl="/" backLinkForce={true} />
             <Block strong>
                 <Row nogap>
                     {question.title}
                 </Row>
             </Block>
             <Row>
-                <img src={"images/"+question._id.$oid+".jpg"}/>
+                <img src={"images/" + question._id.$oid + ".jpg"} height={500}/>
             </Row>
-                <Row>
-                    <Col width="5"></Col>
-                    <Col width="25">
-                        <Stepper value={answer} min={10} max={99} step={1} autorepeat={true}
-                                 onStepperChange={(e) => setAnswer(e)}/>
-                        {"." + Math.round((question.price.amount * 100) % 100) + " €"}
-                    </Col>
-                    <Col width="50">
-                        <Button fill raised href={"/answer/" + answer + "/"}>Valider</Button>
-                    </Col>
-                </Row>
+            <Row>
+                <Col width="5"></Col>
+                <Col width="25">
+                    <Stepper value={answer} min={10} max={99} step={1} autorepeat={true}
+                        onStepperChange={(e) => setAnswer(e)} />
+                    {"." + Math.round((question.price.amount * 100) % 100) + " €"}
+                </Col>
+                <Col width="50">
+                    <Button fill raised href={"/answer/" + answer + "/"}>Valider</Button>
+                </Col>
+            </Row>
         </Page>
     );
 }

d334ea1d0c723180aee1b93c7e97ef61287d69a3
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-05-24 17:15:47 +0200

    Propriété style

--- src/components/tyrolien.jsx
+++ src/components/tyrolien.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 
 const Tyrolien = (props) => {
-    let topPosition = 270;
-    let leftPosition = 80;
 
-    if (props.actuel <= props.max) {
-        topPosition = 270 - (props.actuel/props.max) * (270-150);
-        leftPosition = 80 + (props.actuel/props.max) * (470-80);
-    } else {
-        topPosition = 300;
-        leftPosition = 520;
-        const rotate = "90deg";
+    const style = {
+        position: "absolute",
+        top: props.actuel <= props.max ? 270 - (props.actuel/props.max) * (270-150) : 300,
+        left: props.actuel <= props.max ? 80 + (props.actuel/props.max) * (470-80) : 520,
+        transform: props.actuel <= props.max ? '' : 'rotate(90deg)'
     }
 
     return(
         <div style={{position: "relative", top: 0, left: 0}}>
             <img style={{position: "relative", top: 0, left: 0}} height={500} z-index={1} src="/images/tyrolien.png" />
-            <img style={{position: "absolute", top: topPosition, left: leftPosition}} height={50} z-index={2} src="/images/personnage.png" />
+            <img style={style} height={50} z-index={2} src="/images/personnage.png" />
         </div>
     )
 }
